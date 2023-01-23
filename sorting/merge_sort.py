ff65709aee00bda503a00182d04b8fefdc4ac4dc
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-01-23 08:04:18 +0100

    optimize merge sort code

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -1,68 +1,63 @@
 class Solution:
-    def merge(self, arr, l, m, r):
-        i = l
-
-        """use a temporary array to store elements in sorted order from divided arrays.
-           return back the elements in the original array from temp array."""
-
-        temp = [0] * 100000
-
-        i = l # starting index of left half of arr
-        j = m + 1;   # starting index of right half of arr
-        f = l ;        # index used to transfer elements in temporary array
-
-
-        # storing elements in the temporary array in a sorted manner
-
-        while i <= m and j <= r:
-            if arr[i] < arr[j]:
-                temp[f] = arr[i]
-                i+= 1
-            
-            else:
-                temp[f] = arr[j]
-                j += 1
-
-            f += 1
-
-        # if elements on the right half are still left
-
-        if i > m:
-            while j <= r:
-                temp[f] = arr[j]
-                f += 1
-                j += 1
+    def merge(self,arr, l, m, r): 
+        """
+        used to merge the 2 halves of the array. It assumes that 
+        both part of the array are sorted and merges both of them
+        """ 
+        # code here
+        list1 = arr[l:m+1]
+        list2 = arr[m+1:r+1]
         
-        # if elements on the left half are still left
-        else:
-
-            while i <= m:
-                temp[f] = arr[i]
-                f += 1
+        l1 = len(list1)
+        l2 = len(list2)
+        
+        i=j=0  # initializing left arr copy and right arr copy counters
+        k = l  # initializing inplace to-be sorted array counter
+               # "k" recursively needs to be initialized to the passed "l"
+        
+        # storing elements in the copy arrays in a sorted manner
+        while i<l1  and j <l2:
+            if list1[i] <= list2[j]:
+                arr[k] = list1[i]
                 i += 1
-
-
-        # transfering all elements from temp to arr
-
-        for f in range(l, r+1):
-            arr[f] = temp[f]
-
+            else:
+                arr[k] = list2[j]
+                j += 1
+            k+=1
+            
+            
+        # if elements on the left half are still left
+        while i < l1:
+            arr[k] = list1[i]
+            i += 1
+            k += 1
         
-
-    # Divide the array around the middle element by making the recursive call
-    def mergeSort(self, arr, l, r):
-        if l < r:
-            m = (l+r)//2
-
-            # left half
-            self.mergeSort(arr, l, m)
-            # right half
-            self.mergeSort(arr, m+1, r)
-
-            # merge sorted halves
-            self.merge(arr, l, m, r)
-
+        # if elements on the right half are still left
+        while j < l2:
+            arr[k] = list2[j]
+            j += 1
+            k += 1
+            
         return arr
+                
+    def mergeSort(self,arr, l, r):
+        """
+        divides the array into 2 parts . l to m and m+1 to r
+        """
+        #code here
+
+        if l < r:
+            
+            m = (l+r) // 2
+            
+            self.mergeSort(arr, l, m)
+            
+            self.mergeSort(arr, m+1, r)
+            
+            self.merge(arr, l, m, r)
+            
+        return arr
+
 
 
 
