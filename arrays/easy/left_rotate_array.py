dedfb52710ffe968ce7b7a1c81869723c2cbe9f4
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-01-26 07:46:34 +0100

    add left rotate array code

---
+++ arrays/easy/left_rotate_array.py
@@ -0,0 +1,30 @@
+class Solution:
+    def leftRotate(self, arr, k, n):
+        temp = [0] * n
+        
+        """
+        if "k" exceeds the length of array "n" then the rotation should be kind of cyclic.
+        """
+
+        mod = k % n        
+        
+        for i in range(0, n):
+            temp[i] = arr[(i + mod) % n]
+            
+        for i in range(n):
+            arr[i] = temp[i]
+            
+        return arr
+
+
+
+
+
+
+
+
+
+# test
+ob = Solution()
+ans = ob.leftRotate(arr=[1, 2, 3, 4, 5, 6, 7], k=4, n=7)
+print(ans)
