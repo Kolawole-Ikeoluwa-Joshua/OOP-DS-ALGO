2b853fe63a373dbd33856c0ef73ac1c84d322166
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2023-02-13 08:53:59 +0100

    refactor binary search 1D array code

--- binary search/1D arrays/binary_search_find_x_sorted_array.py
+++ binary search/1D arrays/binary_search_find_x_sorted_array.py
@@ -10,10 +10,6 @@ bs rule of thumb:
 
 - Include ALL possible answers when initialize lo & hi
 - Don't overflow the mid calculation
-if array elements even:
-mid = lo + (hi - lo) // 2, (lower mid)
-mid = lo + (hi - lo + 1) // 2 (upper mid)
-
 - Shrink boundary using a logic that will exclude mid
 - Avoid infinity loop by picking the correct mid and shrinking logic
 - Always think of the case when there are 2 elements left
@@ -24,19 +20,18 @@ class Solution:
         hi = len(nums) - 1
 
         while lo < hi:
-            mid = lo + (hi - lo + 1) // 2
+            mid = lo + (hi - lo) // 2
 
-            if target < nums[mid]:
-                hi = mid -1
+            if nums[mid] == target:
+                return mid 
+            
+            elif nums[mid] > target:
+                hi = mid - 1
 
             else:
-                lo = mid
+                lo = mid + 1
 
-        return lo if nums[lo] == target else -1
-
-
-
-
+        return -1
 
 
 
